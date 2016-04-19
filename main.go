601b233d69140517165ff0524b65a81d4a7a0553
truemate <truemate@nowhere.net>
2016-04-19 14:42:47 +1000

    1. Implementation of the RabbitMQ sender/publisher \r\n 2. Implementation of the POST request handler for the RabbitMQ publisher

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+  "bytes"
+  "io"
+)
 
 func main() {
   // ServerMux
   http.HandleFunc("/", hello)
+  http.HandleFunc("/send", send)
   http.ListenAndServe(":8080", nil)
 }
 
@@ -14,4 +19,16 @@ func hello(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("<h1>Hello!</h1>"))
   w.Write([]byte("</body>"))
   w.Write([]byte("</html>\r\n"))
+}
+
+func send(w http.ResponseWriter, r *http.Request) {
+  //decoder := json.NewDecoder(req.Body)
+  send_message(streamToString(r.Body))
+  w.Write([]byte("{\"message\":\"Sent Succesfully!\"}"))
+}
+
+func streamToString(stream io.Reader) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stream)
+	return buf.String()
 }
